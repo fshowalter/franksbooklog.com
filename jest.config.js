6d862a39d44346450291fc8ab210fff02fbbd130
Frank Showalter <fshowalter@gmail.com>
2022-07-06 12:44:09 -0400

    Revert "try/fix tests"
    This reverts commit 0a561e9c321bd398856cab2dd0b47bca986c5d4c.

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,12 @@
-const { compilerOptions } = require("./tsconfig.json");
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
-const paths = pathsToModuleNameMapper(compilerOptions.paths, {
-  prefix: "<rootDir>/",
-});
-
 module.exports = {
   transform: {
     "^.+\\.[jt]sx?$": "<rootDir>/jest-preprocess.js",
   },
   moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
-    ...paths,
+    ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/file-mock.js",
+    "^gatsby-page-utils/(.*)$": `gatsby-page-utils/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
   },
   testPathIgnorePatterns: [
     "node_modules",
