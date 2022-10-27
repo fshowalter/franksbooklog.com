0956c5d91a4e14261de9e79108ae5b5fcd1b6d12
Frank Showalter <fshowalter@gmail.com>
2022-10-27 15:23:48 -0400

    update homepage

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -210,7 +210,7 @@ export const pageQuery = graphql`
           slug
           sequence
         }
-        dateFinished(formatString: "DD MMM, YYYY")
+        dateFinished(formatString: "DD MMM YYYY")
         linkedExcerpt
         reviewedWork {
           title
