ca7e98a29da11b4bc01dd4b1c405f886a85a5af3
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-02-14 17:22:50 -0500

    remove dead code

--- src/api/data/utils/nullable.ts
+++ src/api/data/utils/nullable.ts
@@ -1,9 +1,5 @@
 import { z } from "zod";
 
-export function nullableNumber() {
-  return z.nullable(z.number()).transform((data) => data ?? undefined);
-}
-
 export function nullableString() {
   return z.nullable(z.string()).transform((data) => data ?? undefined);
 }
