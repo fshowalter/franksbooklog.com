ae85365f2c4bdec3589b290a98ccbbc45a075b1f
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-07-17 21:54:30 -0400

    wip

--- src/components/Home/HomeListItem.tsx
+++ src/components/Home/HomeListItem.tsx
@@ -64,12 +64,14 @@ export function HomeListItem({
           <div
             className={`
               relative z-10
-              before:absolute before:top-0 before:left-0 before:block
-              before:size-full before:bg-[url(/assets/spine-light.png)]
+              before:absolute before:top-0 before:left-0 before:z-10
+              before:block before:size-full before:rounded-[2.5px]
+              before:bg-[url(/assets/spine-light.png)]
               before:bg-size-[100%_100%]
-              after:absolute after:top-0 after:left-0 after:block
-              after:size-full after:bg-[url(/assets/spine-dark.png)]
-              after:bg-size-[100%_100%] after:mix-blend-multiply
+              after:absolute after:top-0 after:left-0 after:z-10 after:block
+              after:size-full after:rounded-[2.5px]
+              after:bg-[url(/assets/spine-dark.png)] after:bg-size-[100%_100%]
+              after:mix-blend-multiply
             `}
           >
             <Cover
@@ -79,7 +81,7 @@ export function HomeListItem({
               alt={`A cover of ${value.title} by ${toSentenceArray(
                 value.authors.map((a) => a.name),
               ).join("")}`}
-              className="rounded-[2.5px]"
+              className="rounded-[2.5px] bg-default"
               loading={eagerLoadCoverImage ? "eager" : "lazy"}
             />
           </div>
