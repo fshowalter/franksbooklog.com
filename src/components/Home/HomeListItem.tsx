083c1c393fe1f462425f5bc7424542e8bf308210
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-07-19 16:23:43 -0400

    sizes

--- src/components/Home/HomeListItem.tsx
+++ src/components/Home/HomeListItem.tsx
@@ -11,7 +11,7 @@ import { toSentenceArray } from "~/utils";
 export const CoverImageConfig = {
   height: 372,
   sizes:
-    "(min-width: 1840px) 248px, (min-width: 1300px) calc(13.08vw + 10px), (min-width: 1260px) calc(-360vw + 4784px), (min-width: 900px) calc(19.12vw + 11px), (min-width: 600px) 27.5vw, calc(41.43vw + 8px)",
+    "(min-width: 1860px) 200px, (min-width: 1440px) calc(9.75vw + 21px), (min-width: 1280px) calc(16.43vw - 59px), (min-width: 1040px) calc(6.36vw + 120px), (min-width: 960px) 200px, (min-width: 780px) calc(11.25vw + 94px), (min-width: 620px) 200px, (min-width: 460px) calc(25.71vw + 46px), calc(42.14vw - 12px)",
   width: 248,
 };
 
