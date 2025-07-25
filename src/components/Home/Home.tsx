0e078cec132a09f6549a08c5eadb8c5b10a3425b
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-07-25 17:42:59 -0400

    wip

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -39,8 +39,8 @@ export function Home({ backdropImageProps, deck, values }: Props): JSX.Element {
             -mx-4 grid grid-cols-2 grid-rows-[auto_1fr] flex-wrap gap-x-2
             gap-y-2
             tablet:flex tablet:items-baseline tablet:gap-x-4 tablet:gap-y-4
-            laptop:-mx-6 laptop:gap-x-6 laptop:gap-y-6
-            desktop:-mx-8 desktop:gap-y-12
+            laptop:mx-0 laptop:gap-x-10 laptop:gap-y-6
+            desktop:gap-y-12
           `}
         >
           {values.map((value, index) => {
