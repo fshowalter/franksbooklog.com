c1a153cdfa9c4012466232d7e938caef59e3f52c
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-08-31 16:33:42 -0400

    test: simple subtitle text - just 'Test'

--- src/components/Home/OpenGraphImage.tsx
+++ src/components/Home/OpenGraphImage.tsx
@@ -62,7 +62,7 @@ export function OpenGraphImage({
             textShadow: "1px 1px 2px black",
           }}
         >
-          Literature is a relative term.
+          Test
         </div>
       </div>
     </div>
