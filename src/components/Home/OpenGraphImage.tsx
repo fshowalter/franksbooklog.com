c9706658fe0bf8169fe3d69abe68fa56aea56105
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2025-08-31 16:35:24 -0400

    test: subtitle with 'Literature'

--- src/components/Home/OpenGraphImage.tsx
+++ src/components/Home/OpenGraphImage.tsx
@@ -62,7 +62,7 @@ export function OpenGraphImage({
             textShadow: "1px 1px 2px black",
           }}
         >
-          Test
+          Literature
         </div>
       </div>
     </div>
