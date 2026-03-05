b305bcfcea092c151c832cd02bfd90235ab1f5d8
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-03-05 17:24:19 -0500

    wip

--- src/utils/grades.ts
+++ src/utils/grades.ts
@@ -5,6 +5,22 @@
 export const GRADE_MIN = 2;
 export const GRADE_MAX = 16;
 
+export const GRADE_TO_SVG_FILE_AND_ALT: Record<string, [string, string]> = {
+  A: ["/svg/5-stars.svg", "5 stars (out of 5)"],
+  "A+": ["/svg/5-stars.svg", "5 stars (out of 5)"],
+  "A-": ["/svg/4-half-stars.svg", "4.5 stars (out of 5)"],
+  B: ["/svg/4-stars.svg", "4 stars (out of 5)"],
+  "B+": ["/svg/4-stars.svg", "4 stars (out of 5)"],
+  "B-": ["/svg/3-half-stars.svg", "3.5 stars (out of 5)"],
+  C: ["/svg/3-stars.svg", "3 stars (out of 5)"],
+  "C+": ["/svg/3-stars.svg", "3 stars (out of 5)"],
+  "C-": ["/svg/2-half-stars.svg", "2.5 stars (out of 5)"],
+  D: ["/svg/2-stars.svg", "2 stars (out of 5)"],
+  "D+": ["/svg/2-stars.svg", "2 stars (out of 5)"],
+  "D-": ["/svg/1-half-stars.svg", "1.5 stars (out of 5)"],
+  F: ["/svg/1-star.svg", "1 star (out of 5)"],
+};
+
 const GRADE_TO_LETTER: Record<number, string> = {
   2: "F-",
   3: "F",
