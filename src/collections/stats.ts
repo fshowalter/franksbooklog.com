73f457b0d8e138d7b227e9629bac4f9fdd7f7728
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-03-13 15:56:19 -0400

    deprecations

--- src/collections/stats.ts
+++ src/collections/stats.ts
@@ -1,5 +1,5 @@
+import { z } from "astro/zod";
 import { defineCollection } from "astro:content";
-import { z } from "astro:content";
 import path from "node:path";
 
 import { CONTENT_ROOT } from "./contentRoot";
