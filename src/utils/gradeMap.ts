3a1a50640aebf7816a92b494be2f688b9c6ebe98
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-03-27 17:23:01 -0400

    `fix: correct grade ordering and pluralization in grade utils`

--- src/utils/gradeMap.ts
+++ src/utils/gradeMap.ts
@@ -17,6 +17,6 @@ export const gradeMap = {
   "D+": ["/svg/2-stars.svg", "2 stars (out of 5)"],
   "D-": ["/svg/1-half-stars.svg", "1.5 stars (out of 5)"],
   F: ["/svg/1-star.svg", "1 star (out of 5)"],
-  "F+": ["/svg/1-star.svg", "1 stars (out of 5)"],
+  "F+": ["/svg/1-star.svg", "1 star (out of 5)"],
   "F-": ["/svg/half-star.svg", "1/2 star (out of 5)"],
 };
