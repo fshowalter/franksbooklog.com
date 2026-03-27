25c5dfa296184d7bf2a0f7a1ab3f7606b82d5bfb
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-03-26 21:57:44 -0400

    `refactor: sort mostRecentReviewedTitles by reviewSequence instead of reviewDate`

--- src/utils/mostRecentReviewedTitles.ts
+++ src/utils/mostRecentReviewedTitles.ts
@@ -4,7 +4,7 @@ export async function mostRecentReviewedTitles(limit: number) {
   const reviewedTitles = await getCollection("reviewedTitles");
 
   reviewedTitles.sort((a, b) => {
-    return b.data.reviewDate.getTime() - a.data.reviewDate.getTime();
+    return b.data.reviewSequence.localeCompare(a.data.reviewSequence);
   });
 
   return reviewedTitles.slice(0, limit);
